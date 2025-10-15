88424a016beb539fe7424fabc5526541601b336a
Akshata Hegde <123236828+AkshataKrishnaHegde@users.noreply.github.com>
2025-10-15 22:01:20 +0530

    Update ChatPage.jsx

--- frontend/src/routes/chatPage/ChatPage.jsx
+++ frontend/src/routes/chatPage/ChatPage.jsx
@@ -5,16 +5,18 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 import Markdown from "react-markdown";
 import { IKImage } from "imagekitio-react";
+import { useAuth } from "@clerk/clerk-react";
 
 const ChatPage = () => {
   const path = useLocation().pathname;
   const chatId = path.split("/").pop();
-
+  const { getToken } = useAuth();
   const { isLoading, error, data } = useQuery({
     queryKey: ["chat", chatId],
     queryFn: () =>
       fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
         credentials: "include",
+        Authorization: `Bearer ${token}`
       }).then((res) => res.json()),
   });
 
