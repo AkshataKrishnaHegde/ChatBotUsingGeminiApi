739650a3fdd42cfa921bebb6a75f6a18af8ae513
Akshata Hegde <123236828+AkshataKrishnaHegde@users.noreply.github.com>
2025-10-15 21:35:15 +0530

    Update NewPrompt.jsx

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
+import { useAuth } from "@clerk/clerk-react";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
 import model from "../../lib/gemini";
 import Markdown from "react-markdown";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+const { getToken } = useAuth();
 const NewPrompt = ({ data }) => {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
@@ -43,6 +45,7 @@ const NewPrompt = ({ data }) => {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify({
           question: question.length ? question : undefined,
